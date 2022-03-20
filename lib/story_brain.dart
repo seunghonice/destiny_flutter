e1b072d2607d606aa8d3f597c8caa0c22a7a12ba
seunghonice <seunghonice@gmail.com>
2022-03-20 23:53:39 +0900

    feat: 오징어게임 형식으로 변경했다.

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -2,36 +2,47 @@ import 'package:destiny_flutter/story.dart';
 
 class StoryBrain {
   int _storyNumber = 0;
+  List<int> answer = [1, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2];
+  List<int> userAnswer = [];
   final List<Story> _storyData = [
+    Story(storyTitle: '오징어 다리 건너기 게임..', choice1: '시작', choice2: ''),
+    Story(storyTitle: '오징어 다리 건너기 게임..\n-\n-', choice1: '윗다리', choice2: '아랫다리'),
     Story(
-        storyTitle:
-            'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
-        choice1: 'I\'ll hop in. Thanks for the help!',
-        choice2: 'Better ask him if he\'s a murderer first.'),
+        storyTitle: '오징어 다리 건너기 게임..\nO-\nX-', choice1: '윗다리', choice2: '아랫다리'),
     Story(
-        storyTitle: 'He nods slowly, unphased by the question.',
-        choice1: 'At least he\'s honest. I\'ll climb in.',
-        choice2: 'Wait, I know how to change a tire.'),
+        storyTitle: '오징어 다리 건너기 게임..\nOX-\nXO-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
     Story(
-        storyTitle:
-            'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
-        choice1: 'I love Elton John! Hand him the cassette tape.',
-        choice2: 'It\'s him or me! You take the knife and stab him.'),
+        storyTitle: '오징어 다리 건너기 게임..\nOXO-\nXOX-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
     Story(
-        storyTitle:
-            'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
-        choice1: 'Restart',
-        choice2: ''),
+        storyTitle: '오징어 다리 건너기 게임..\nOXOX-\nXOXO-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
     Story(
-        storyTitle:
-            'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
-        choice1: 'Restart',
-        choice2: ''),
+        storyTitle: '오징어 다리 건너기 게임..\nOXOXO-\nXOXOX-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
     Story(
-        storyTitle:
-            'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
-        choice1: 'Restart',
-        choice2: '')
+        storyTitle: '오징어 다리 건너기 게임..\nOXOXOX-\nXOXOXO-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
+    Story(
+        storyTitle: '오징어 다리 건너기 게임..\nOXOXOXO-\nXOXOXOX-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
+    Story(
+        storyTitle: '오징어 다리 건너기 게임..\nOXOXOXOX-\nXOXOXOXO-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
+    Story(
+        storyTitle: '오징어 다리 건너기 게임..\nOXOXOXOXO-\nXOXOXOXOX-',
+        choice1: '윗다리',
+        choice2: '아랫다리'),
+    Story(storyTitle: '오징어 다리 건너기 게임..\nOXOXOXOXOX\nXOXOXOXOXO\n우승!', choice1: '처음으로', choice2: ''),
+    Story(storyTitle: '죽음!!!', choice1: '처음으로', choice2: '')
   ];
 
   String getStory() => _storyData[_storyNumber].storyTitle;
@@ -41,21 +52,15 @@ class StoryBrain {
   String getChoice2() => _storyData[_storyNumber].choice2;
 
   void nextStory(int choiceNumber) {
-    switch (_storyNumber) {
-      case 0:
-        _storyNumber = choiceNumber == 1 ? 2 : 1;
-        break;
-      case 1:
-        _storyNumber = choiceNumber == 1 ? 2 : 3;
-        break;
-      case 2:
-        _storyNumber = choiceNumber == 1 ? 5 : 4;
-        break;
-      default:
-        _storyNumber = 0;
-        break;
+    userAnswer.add(choiceNumber);
+    if (_storyNumber > 10) {
+      _storyNumber = 0;
+    } else if (userAnswer.last != answer[_storyNumber]) {
+      _storyNumber = 12;
+    } else {
+      _storyNumber++;
     }
   }
 
-  bool buttonShouldBeVisible() => _storyNumber < 3 ? true : false;
+  bool buttonShouldBeVisible() => getChoice2() != '';
 }
